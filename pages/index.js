ffb61b7daa06c827b4a3a95f37dde7b111e29aaa
Muhammad Syawaludin <syawalhastaf@gmail.com>
2020-09-11 16:09:12 +0800

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,82 @@
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [digimonList, setDigimonList] = useState([]);
+  const [error, setError] = useState('');
+  const getDigimon = async () => {
+    let url = 'https://digimon-api.vercel.app/api/digimon';
+    fetch(url)
+      .then(
+        function (response) {
+          if (response.status !== 200) {
+            setError('Looks like there was a problem. Status Code: ' + response.status);
+            return;
+          }
+          // Examine the text in the response
+          response.json().then(function (data) {
+            setDigimonList(data);
+          });
+        }
+      )
+      .catch(function (err) {
+        setError('Fetch Error :-S', err);
+      });
+  }
+
+  useEffect(() => {
+    getDigimon();
+  }, []);
+
+  const dt = new Date();
+  const checkStatus = error != '' ? error : 'Loading ...';
+
   return (
     <div className={styles.container}>
+
       <Head>
-        <title>Create Next App</title>
+        <title>Digimon Card List</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Digimon Card List
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {digimonList.length === 0 ? <p>{checkStatus}</p> :
+            digimonList.map((obj, index) =>
+              <div key={index} className={styles.card}>
+                <p>{obj.name}</p>
+                <img className={styles.digimonImg} src={obj.img} />
+                <p>{obj.level}</p>
+              </div>
+            )
+          }
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {' '}Powered by{' '}
+            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://www.linkedin.com/in/muhammadsyawaludin/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            &nbsp;- Copyright © {dt.getFullYear()} شَوَّال‎
+          </a>
+        </p>
       </footer>
     </div>
   )
